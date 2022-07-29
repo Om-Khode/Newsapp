fbffa016927cb92dfbd03fec4300000d8c20510e
Om-Khode <99012784+Om-Khode@users.noreply.github.com>
2022-07-29 12:11:48 +0530

    Some minor changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,9 +6,9 @@ function Navbar() {
     return (
         <div className="navbar">
             <p className="name"><i> NEWS </i></p>
-            <a href="#"><p className="home">Home</p></a>
-            <a href="#"><p className="contactUs">Contact Us</p></a>
-            <a href="#"><p className="about">About</p></a>
+            <a href="/"><p className="home">Home</p></a>
+            <a href="/"><p className="contactUs">Contact Us</p></a>
+            <a href="/"><p className="about">About</p></a>
         </div>
     );
 }
