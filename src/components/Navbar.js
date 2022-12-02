ea63aa71198ae1d543a389920d7efcab6b527bcd
Om-Khode <99012784+Om-Khode@users.noreply.github.com>
2022-12-02 23:04:40 +0530

    Added More

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,26 @@
 import React from "react";
-import './Navbar.css'
+import "./Navbar.css";
 
 function Navbar() {
-    
-    return (
-        <div className="navbar">
-            <p className="name"><i> NEWS </i></p>
-            <a href="/"><p className="home">Home</p></a>
-            <a href="/"><p className="contactUs">Contact Us</p></a>
-            <a href="/"><p className="about">About</p></a>
-        </div>
-    );
+  return (
+    <div className="navbar">
+      <p className="name">
+        <i> NEWS </i>
+      </p>
+      <a href="/">
+        <p className="home">Home</p>
+      </a>
+      <a href="/">
+        <p className="contactUs">Contact Us</p>
+      </a>
+      <a href="/">
+        <p className="about">About</p>
+      </a>
+      <a href="/">
+        <p className="More">More</p>
+      </a>
+    </div>
+  );
 }
 
 export default Navbar;
